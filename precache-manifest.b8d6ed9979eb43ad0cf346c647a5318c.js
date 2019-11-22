11376b260e01b42877f84b8d02d7c95d014fcb76
hannaharnold <hannaharnold98@yahoo.com>
2019-11-22 16:02:25 +0000

    Updates

---
+++ precache-manifest.b8d6ed9979eb43ad0cf346c647a5318c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3092b005cb52ea2f639e075f73df1144",
+    "url": "/calculator/./index.html"
+  },
+  {
+    "revision": "87b14c47aa70604a3820",
+    "url": "/calculator/./static/css/main.a134f859.chunk.css"
+  },
+  {
+    "revision": "4ba402bb1338bd04274a",
+    "url": "/calculator/./static/js/2.5c58a3f5.chunk.js"
+  },
+  {
+    "revision": "87b14c47aa70604a3820",
+    "url": "/calculator/./static/js/main.7734fef7.chunk.js"
+  },
+  {
+    "revision": "c11f1ad5c5db417d6f83",
+    "url": "/calculator/./static/js/runtime-main.a88f91be.js"
+  }
+]);
